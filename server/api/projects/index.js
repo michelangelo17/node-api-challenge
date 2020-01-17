bc42c83649b89f73b3fed0da577038308a96b9d2
michelangelo17 <michelangelo.markus@gmail.com>
2020-01-17 09:59:36 -0800

    projects api fully functional

--- server/api/projects/index.js
+++ server/api/projects/index.js
@@ -17,10 +17,17 @@ router.get('/:id', validateId(db), (req, res) =>
     .catch(err => res.status(500).json({ message: err.message }))
 )
 
+router.get('/:id/actions', validateId(db), (req, res) =>
+  db
+    .getProjectActions(req.params.id)
+    .then(actionsArray => res.status(200).json(actionsArray))
+    .catch(err => res.status(500).json({ message: err.message }))
+)
+
 router.post('/', validateProjectReq, (req, res) =>
   db
     .insert(req.body)
-    .then(newProject => res.status(200).json(newProject))
+    .then(newProject => res.status(201).json(newProject))
     .catch(err => res.status(500).json({ message: err.message }))
 )
 
@@ -28,6 +35,13 @@ router.put('/:id', validateId(db), validateProjectReq, (req, res) =>
   db
     .update(req.params.id, { ...req.body })
     .then(updatedProject => res.status(200).json(updatedProject))
+    .catch(err => res.status(500).json({ message: err.message }))
+)
+
+router.delete('/:id', validateId(db), (req, res) =>
+  db
+    .remove(req.params.id)
+    .then(numDeleted => res.status(200).end())
     .catch(err => res.status(500).json({ message: err.message }))
 )
 
