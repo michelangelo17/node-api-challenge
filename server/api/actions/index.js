1b7fb663b163a6f860a9920d0664dc5cc8ceefe1
michelangelo17 <michelangelo.markus@gmail.com>
2020-01-17 10:54:32 -0800

    all enpoints live

--- server/api/actions/index.js
+++ server/api/actions/index.js
@@ -1,6 +1,42 @@
-const { handle500 } = require('../../middleware/apiMW')
-const db = require('../../../data/helpers/projectModel')
+const { handle500, validateId } = require('../../middleware/apiMW')
+const { validateActionReq } = require('../../middleware/apiMW/actionsMW')
+const db = require('../../../data/helpers/actionModel')
 const router = require('express').Router()
+
+router.get('/', (req, res) =>
+  db
+    .get()
+    .then(actionsArray => res.status(200).json(actionsArray))
+    .catch(err => res.status(500).json({ message: err.message }))
+)
+
+router.get('/:id', validateId(db), (req, res) =>
+  db
+    .get(req.params.id)
+    .then(action => res.status(200).json(action))
+    .catch(err => res.status(500).json({ message: err.message }))
+)
+
+router.post('/', validateActionReq, (req, res) =>
+  db
+    .insert(req.body)
+    .then(newAction => res.status(201).json(newAction))
+    .catch(err => res.status(500).json({ message: err.message }))
+)
+
+router.put('/:id', validateId(db), validateActionReq, (req, res) =>
+  db
+    .update(req.params.id, { ...req.body })
+    .then(updatedAction => res.status(200).json(updatedAction))
+    .catch(err => res.status(500).json({ message: err.message }))
+)
+
+router.delete('/:id', validateId(db), (req, res) =>
+  db
+    .remove(req.params.id)
+    .then(numDeleted => res.status(200).end())
+    .catch(err => res.status(500).json({ message: err.message }))
+)
 
 router.use(handle500)
 
---
+++ server/middleware/apiMW/actionsMW/index.js
@@ -0,0 +1,37 @@
+const validateActionReq = (req, res, next) =>
+  JSON.stringify(req.body) !== '{}'
+    ? req.body.project_id
+      ? req.method === 'POST'
+        ? req.body.description
+          ? req.body.description.length <= 128
+            ? req.body.notes
+              ? req.body.project_id
+                ? next()
+                : res
+                    .status(400)
+                    .json({ message: 'missing required project_id field' })
+              : res
+                  .status(400)
+                  .json({ message: 'missing required notes field' })
+            : res.status(400).json({
+                message: 'description must be 128 characters or fewer',
+              })
+          : res
+              .status(400)
+              .json({ message: 'missing required description field' })
+        : req.body.description || req.body.notes
+        ? req.body.description
+          ? req.body.description.length <= 128
+            ? next()
+            : res.status(400).json({
+                message: 'description must be 128 characters or fewer',
+              })
+          : next()
+        : res.status(400).json({
+            message:
+              'Must include at least one of the following fields: description, notes',
+          })
+      : res.status(400).json({ message: 'missing required project_id field' })
+    : res.status(400).json({ message: 'missing project data' })
+
+module.exports = { validateActionReq }
